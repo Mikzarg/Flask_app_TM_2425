fbf03d9c3c3a2df64cf92888c2f319a0cc277b00
Mikzarg <180762134+Mikzarg@users.noreply.github.com>
2024-12-19 13:06:51 +0100

    HTML CSS de la page Tournoi

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,30 @@
-console.log("Script bien chargé")
+function changerBouton() {
+    var button = document.getElementById("btnConnect");
+
+    // Vérifie si le bouton a déjà été cliqué
+    if (button.classList.contains("clicked")) {
+        // Réinitialise l'état du bouton
+        button.classList.remove("clicked");
+        button.innerHTML = '<h3 id="sinscrire">S\'INSCRIRE</h3>';
+    } else {
+        // Ajoute l'état "cliqué"
+        button.classList.add("clicked");
+
+        // Ajouter du texte ou d'autres boutons à l'intérieur du bouton
+        var addedContent = `
+            <h3 id="sinscrire">S'INSCRIRE</h3>
+            <p id="text-test">Souhaitez-vous participer au <br>Tournoi d’échecs CSUD 2024?</p>
+            <div id="div-test">
+              <button class="bouton-test" id="bouton-test-confirmer" onclick="alert('Premier bouton cliqué!')">
+                <img class="image-logo" src="{{url_for('static', filename='imgs/vu.png')}}" alt="vu">
+                <span>CONFIRMER</span>
+              </button>
+              <button class="bouton-test" id="bouton-test-annuler" onclick="alert('Deuxième bouton cliqué!')">
+                <img class="image-logo" src="{{url_for('static', filename='imgs/croix.png')}}" alt="croix">
+                <span>ANNULER</span>
+              </button>
+            </div>
+        `;
+        button.innerHTML = addedContent; // Ajoute les nouveaux éléments dans le bouton
+    }
+}
--- app/views/tournament.py
+++ app/views/tournament.py
@@ -9,3 +9,7 @@ tournament_bp = Blueprint('tournament', __name__, url_prefix='/tournament')
 def show_tournament():
     # Affichage de la page des tournois
     return render_template('tournament/tournament.html')
+@tournament_bp.route('/precedents', methods=('GET', 'POST'))
+def show_precedents():
+    # Affichage de la page des membres
+    return render_template('tournament/precedents.html')
