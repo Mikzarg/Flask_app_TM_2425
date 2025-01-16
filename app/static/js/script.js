1a47256e610b968547eae1e8fed048bec81c0cb6
Mikzarg <180762134+Mikzarg@users.noreply.github.com>
2025-01-16 18:20:42 +0100

    Cartes des utilisateurs de la page Membres

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,18 +1,18 @@
-function changerBouton() {
+function changerBoutonSinscrire() {
     var button = document.getElementById("btnConnect");
 
     // Vérifie si le bouton a déjà été cliqué
     if (button.classList.contains("clicked")) {
         // Réinitialise l'état du bouton
         button.classList.remove("clicked");
-        button.innerHTML = '<h3 id="sinscrire">S\'INSCRIRE</h3>';
+        button.innerHTML = '<h3>S\'INSCRIRE</h3>';
     } else {
         // Ajoute l'état "cliqué"
         button.classList.add("clicked");
 
         // Ajouter du texte ou d'autres boutons à l'intérieur du bouton
         var addedContent = `
-            <h3 id="sinscrire">S'INSCRIRE</h3>
+            <h3 id="sinscriree">S'INSCRIRE</h3>
             <p id="text-test">Souhaitez-vous participer au <br>Tournoi d’échecs CSUD 2024?</p>
             <div id="div-test">
               <button class="bouton-test" id="bouton-test-confirmer" onclick="alert('Premier bouton cliqué!')">
@@ -28,3 +28,71 @@ function changerBouton() {
         button.innerHTML = addedContent; // Ajoute les nouveaux éléments dans le bouton
     }
 }
+function changerBoutonListe() {
+  var button = document.getElementById("bouton-sinscrire-tournoi");
+
+  // Vérifie si le bouton a déjà été cliqué
+  if (button.classList.contains("clicked")) {
+      // Réinitialise l'état du bouton
+      button.classList.remove("clicked");
+      button.innerHTML = '<h3>LISTE DES PARTICIPANTS</h3>';
+  } else {
+      // Ajoute l'état "cliqué"
+      button.classList.add("clicked");
+
+      // Ajouter du texte ou d'autres boutons à l'intérieur du bouton
+      var addedContent = `
+        <h3 >LISTE DES PARTICIPANTS</h3>
+        <table id="tournoi-liste-tableau">
+          <tr>
+            <th class="th-liste">1. </th>
+            <td class="td-liste">Annette Black</td>
+          </tr>
+          <tr>
+            <th class="th-liste">2. </th>
+            <td class="td-liste">Jenny Wilson</td>
+          </tr>
+          <tr>
+            <th class="th-liste">3. </th>
+            <td class="td-liste">Floyd Miles</td>
+          </tr>
+          <tr>
+            <th class="th-liste">4. </th>
+            <td class="td-liste">Arlene McCoy</td>
+          </tr>
+          <tr>
+            <th class="th-liste">5.</th>
+            <td class="td-liste">Eleanor Pena</td>
+          </tr>
+          <tr>
+            <th class="th-liste">6.</th>
+            <td class="td-liste">Darlene Robertson</td>
+          </tr>
+          <tr>
+            <th class="th-liste">7.</th>
+            <td class="td-liste">Dianne Russell</td>
+          </tr>
+          <tr>
+            <th class="th-liste">8.</th>
+            <td class="td-liste">Kristin Watson</td>
+          </tr>
+          <tr>
+            <th class="th-liste">9.</th>
+            <td class="td-liste">Jerome Bell</td>
+          </tr>
+          <tr>
+            <th class="th-liste">10.</th>
+            <td class="td-liste">Leslie Alexander</td>
+          </tr>
+        </table>
+        <img class="fleche" id="fleche-droite" src="{{url_for('static', filename='imgs/right.png')}}" alt="flèche-droite">
+
+      `;
+      button.innerHTML = addedContent; // Ajoute les nouveaux éléments dans le bouton
+  }
+}
+
+
+
+
+
--- app/views/members.py
+++ app/views/members.py
@@ -1,5 +1,6 @@
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 from app.utils import *
+from app.db.db import get_db, close_db
 
 # Routes /members/...
 members_bp = Blueprint('members', __name__, url_prefix='/members')
@@ -8,5 +9,10 @@ members_bp = Blueprint('members', __name__, url_prefix='/members')
 @members_bp.route('/members', methods=('GET', 'POST'))
 @login_required 
 def show_members():
+    db = get_db()
+    admins = db.execute('SELECT * FROM Utilisateurs WHERE role = ?', ('Admin',)).fetchall()
+    members = db.execute('SELECT * FROM Utilisateurs WHERE role = ?', ('Membre',)).fetchall()
+    close_db()
+
     # Affichage de la page des membres
-    return render_template('members/members.html')
+    return render_template('members/members.html', admins=admins, members=members)
